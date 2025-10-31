c5fe667ea4e3782177286cab1af00589195fa851
johnson2123 <johnsonvasupalli@yahoo.com>
2025-11-01 01:05:20 +0530

    first page

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"], // Looks for .html and .js files in the root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
